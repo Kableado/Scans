028bc06f21228b0b9a3255eae3932c34d3231081
Valeriano A.R <valen.alfonso@gmail.com>
2015-06-27 03:00:05 +0200

    Style changes

--- code/scans.php
+++ code/scans.php
@@ -22,15 +22,30 @@ function RenderCommandLog(){
 	global $Commands;
 	$renderLog="";
 	foreach($Commands as $loggedCommand){
-		$renderLog.=RenderParagraph($loggedCommand["Command"],"font-weight: bold;");
+		$renderLog.=RenderParagraph($loggedCommand["Command"],"command");
 		$renderLog.=RenderParagraph($loggedCommand["Result"]);
-		$renderLog.=RenderParagraph($loggedCommand["Error"],"color: red;");
+		$renderLog.=RenderParagraph($loggedCommand["Error"],"error");
 	}
 	
 	return RenderDiv("divConsoleContainer",
 		RenderButton("btnToggle","Log","var elem=document.getElementById('divConsole');if(elem.style.display==''){elem.style.display='none';}else{elem.style.display='';}return false;").
-		RenderDiv("divConsole",$renderLog,"display:none;")
+		RenderDiv("divConsole",$renderLog,null,"display:none;")
 	);
+}
+
+
+function ScannerDetect(){
+	global $ScanImage;
+	$Command=$ScanImage." -f\"%d|%v %m%n\"";
+	$scansResults=ExecCommand($Command);
+	$devicesResults=explode("\n",$scansResults);
+	list($ScannerDevice,$ScannerModel)=explode("|",$devicesResults[0]);
+	
+	$Scanner=array();
+	$Scanner["ScanDevice"]=$ScannerDevice;
+	$Scanner["ScanModel"]=$ScannerModel;
+
+	return $Scanner;
 }
 
 function Scan($device,$resolution,$format,$size,$destFileBase){
@@ -121,15 +136,14 @@ function CropImage($file){
 }
 
 
+
 // Detect scanner
-$CMD=$ScanImage." --list-devices | grep device";
-$SaneScanner = ExecCommand($CMD);
-unset($cmd);
-$start=strpos($SaneScanner,"`")+1;
-$laenge=strpos($SaneScanner,"'")-$start;
-$Scanner = "\"".substr($SaneScanner,$start,$laenge)."\"";
-unset($start);
-unset($laenge);
+$Scanner=array();
+$Scanner["ScanDevice"]=RequestParm("hidScanDevice","");
+$Scanner["ScanModel"]=RequestParm("hidScanDevice","");
+if($Scanner["ScanDevice"]=="" || $Scanner["ScanModel"]==""){
+	$Scanner=ScannerDetect();
+}
 
 
 // Configure with formdata
@@ -144,7 +158,7 @@ $DestFile=null;
 if(RequestParm("btnScan",false)){
 	CleanUp();
 	$baseName="Scan-".date("Y-m-d_H_i_s");
-	$DestFile=Scan($Scanner,$Resolution,$Format,$Size,$baseName);
+	$DestFile=Scan($Scanner["ScanDevice"],$Resolution,$Format,$Size,$baseName);
 	if($Crop){
 		CropImage($DestFile);
 	}
@@ -152,24 +166,30 @@ if(RequestParm("btnScan",false)){
 }
 
 
-echo '<form id="frmMain" method="GET" action="index.php">'."\n";
+echo '<form id="frmMain" method="POST" action="'.$_SERVER['PHP_SELF'].'">'."\n";
 
 // Render header info
-echo RenderFieldInfo("Scanner",$SaneScanner);
+echo RenderFieldInfo("Scanner",$Scanner["ScanModel"]);
 echo RenderFieldCombo("Resolution","ddlResolution",$Resolutions,$Resolution);
 echo RenderFieldCombo("Format","ddlFormat",$Formats,$Format);
 echo RenderFieldCombo("Size","ddlSize",$Sizes,$Size);
-//echo RenderFieldCheckText("Cropping","chkCrop",$Crop,"txtCropFuzz",$CropFuzz);
+echo RenderFieldCheckText("Cropping","chkCrop",$Crop,"txtCropFuzz",$CropFuzz);
 echo RenderFieldButton("","btnScan","Scan");
+echo RenderHidden("hidScanDevice",$Scanner["ScanDevice"]);
+echo RenderHidden("hidScanModel",$Scanner["ScanModel"]);
+
 if($DestFile!=null){
-	$DestFileFixed=htmlentities($DestFile,ENT_HTML5, "UTF-8");
-	echo '<div><a href="'.$DestFileFixed.'">'.
-		'Download '.$DestFileFixed.'</a></div>'."\n";
-	echo '<div><img alt="preview" src="'.$DestFileFixed.'" '.
-		'class="previewImage" /></div>';
+	$DestFilenane=pathinfo($DestFile)["basename"];
+	$DestFileFixed=htmlentities($DestFilenane,ENT_HTML5, "UTF-8");
+	$DestPathFixed=htmlentities($DestFile,ENT_HTML5, "UTF-8");
+	echo '<div><a href="'.$DestPathFixed.'" class="button" download="'.$DestFileFixed.'">'.
+		'Download</a></div>'."\n";
+	echo '<iframe src="'.$DestPathFixed.'" '.
+		'class="previewDoc" ></iframe>';
 }
+
+echo RenderCommandLog();
 
 echo "</form>\n";
 
-echo RenderCommandLog();
 
